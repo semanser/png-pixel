f6a5f82def31623f67e4fb2977d4246f77d6b0b2
JP DeVries <mail@devries.jp>
2016-08-29 00:48:08 +0200

    Async Promise Support
    Closes semanser/png-pixel#1

--- png-pixel.js
+++ png-pixel.js
@@ -3,23 +3,26 @@ const PNG = require('pngjs').PNG;
 const convert = require('color-convert');
 
 PNGPixel = {
-  add: function(input, output ,pixels) {
-    fs.createReadStream(input)
-      .pipe(new PNG({}))
-      .on('parsed', function callback() {
-        pixels.map(pixel => {
-          const idx = (this.width * parseInt(pixel.y, 10) + parseInt(pixel.x, 10)) << 2;
-          const col = convert.hex.rgb(pixel.color);
+  add: function(input, output, pixels) {
+    return new Promise(function(resolve, reject) {
+      fs.createReadStream(input)
+        .pipe(new PNG({}))
+        .on('parsed', function callback() {
+          pixels.map(pixel => {
+            const idx = (this.width * parseInt(pixel.y, 10) + parseInt(pixel.x, 10)) << 2;
+            const col = convert.hex.rgb(pixel.color);
 
-          this.data[idx] = col[0];
-          this.data[idx + 1] = col[1];
-          this.data[idx + 2] = col[2];
-          this.data[idx + 3] = 255;
-          return pixel;
+            this.data[idx] = col[0];
+            this.data[idx + 1] = col[1];
+            this.data[idx + 2] = col[2];
+            this.data[idx + 3] = 255;
+            return pixel;
+          });
+
+          resolve(this.pack().pipe(fs.createWriteStream(output)));
+
         });
-
-        this.pack().pipe(fs.createWriteStream(output));
-      });
+    });
   }
 }
 
